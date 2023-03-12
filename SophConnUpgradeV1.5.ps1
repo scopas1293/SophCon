3b7ba7b51598ee61ee3d8c33086119309e050eec
scopas1293 <127637485+scopas1293@users.noreply.github.com>
2023-03-12 15:55:27 -0400

    Add files via upload

---
+++ SophConnUpgradeV1.5.ps1
@@ -0,0 +1,79 @@
+﻿# Name of software
+$softwareName = "Sophos Connect"
+
+# Check if environment is 64 or 32bit
+$BitVer = ([System.Environment]::Is64BitOperatingSystem)
+
+#x64
+if ($BitVer){
+    # Check if the x64 software is installed
+    $Found = (Get-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+        | Where-Object { $_.DisplayName -eq $softwareName })
+                
+    # Get the software's version number
+    if ($Found){
+        $version = (Get-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+            | Where-Object { $_.DisplayName -eq $softwareName }).DisplayVersion
+    }
+}
+
+#x86
+else{
+    # Check if the x86 software is installed
+    $Found = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+        | Where-Object { $_.DisplayName -eq $softwareName })
+                
+    # Get the software's version number
+    if ($Found){
+        $version = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+            | Where-Object { $_.DisplayName -eq $softwareName }).DisplayVersion
+    }
+}
+
+# If software needs upgrading
+if ($Found){
+
+    # Check if software has been upgraded to required version already, proceed with if not
+    if ($version -ne "2.2.90.1104"){
+    
+        # Check if C:\TE exists 
+        $FolderName = "C:\TE\"
+        if (Test-Path $FolderName) {
+   
+            # If true, doesn't create folder
+        }
+        else
+        {
+            #Create directory if doesn't exist
+            New-Item $FolderName -ItemType Directory
+        }
+
+        # Define new program location
+        $url = "https://github.com/scopas1293/SophCon/raw/main/SC.msi"
+    
+        # Define download location
+        $output = "C:\TE\SC.msi"
+    
+        # Download file to destination
+        Invoke-WebRequest -Uri $url -OutFile $output
+    
+        # Delay to allow for download
+        Start-Sleep -Seconds 30
+    
+        # Set arguments for MSIExec: quiet, log verbose to C:\TE\SC_Log.txt
+        $installArgs = "/qn /l*v C:\TE\SC_log.txt"
+    
+        # Install software with arguements (quite, log at C:\TE\)
+        Start-Process msiexec.exe -ArgumentList "/i `"$output`" $installArgs" -Wait
+        }
+
+    # If software has already been upgraded to required version
+    else {
+        Write-Output "$softwareName has already been upgraded to 2.2.90.1104"
+    }
+}
+
+# If not installed
+else {
+    Write-Output "$softwareName is not installed"
+}
